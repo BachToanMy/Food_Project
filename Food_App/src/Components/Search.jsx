2dbdc8acc32ea2cafa2bd554153874a73d861012
BachToanMy <btmy1314@gmail.com>
2024-03-06 02:33:01 +0700

    06/03/24

---
+++ Food_App/src/Components/Search.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+const URL = "https://api.spoonacular.com/recipes/complexSearch"
+const API_Key = "7cce4dd50e874bd4a8a3334f3393e1e8"
+
+export default function Search({foodData,setFoodData}) {
+  const [query, setQuery] = useState("pizza")
+  //Syntax of the useEffect Hook
+  useEffect(() => {
+    async function fetchFood() {
+    const res = await fetch(`${URL}?query=${query}&apiKey=${API_Key}`);
+      const data = await res.json();
+      console.log(data.results);
+      setFoodData(data.results);
+    }
+    fetchFood();
+  }, [query]);
+  return (
+    <div>
+      <input
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        type="text"
+      ></input>
+    </div>
+  );
+}
