37fd6c523b35715670bd1db5d88ad1cd0807d9bb
BachToanMy <btmy1314@gmail.com>
2024-03-07 14:53:54 +0700

    btm

--- Food_App/src/App.jsx
+++ Food_App/src/App.jsx
@@ -3,14 +3,24 @@ import Search from "./Components/Search";
 import Nav from "./Components/Nav";
 import FoodList from "./Components/FoodList";
 import "./App.css";
+import Container from "./Components/Container";
+import InnerContainer from "./Components/InnerContainer";
+import FoodDetail from "./Components/FoodDetail";
 
 function App() {
   const [foodData, setFoodData] = useState([]);
   return (
     <div className="App">
-      <Nav/>
+      <Nav />
       <Search foodData={foodData} setFoodData={setFoodData} />
-      <FoodList foodData={foodData}/>
+      <Container>
+        <InnerContainer>
+          <FoodList foodData={foodData} />
+        </InnerContainer>
+        <InnerContainer>
+          <FoodDetail/>
+        </InnerContainer>
+      </Container>
     </div>
   );
 }
